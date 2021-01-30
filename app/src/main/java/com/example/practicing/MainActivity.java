fa0fe22c37ca58efee8563b4973f5b3a23e4c09f
Silas <stuartbenza@gmail.com>
2021-01-30 12:48:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/practicing/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.practicing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
